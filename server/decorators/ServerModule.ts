ad6b7a00ed2161ba503fe3a33532c93f36549b13
Christopher <onlygamernerd@gmail.com>
2021-08-07 23:27:19 +0200

    what did I do here?

--- server/decorators/ServerModule.ts
+++ server/decorators/ServerModule.ts
@@ -1,17 +1,17 @@
-import { ServiceMetadata } from "typedi";
-import { ServerModuleToken } from "../core/utils";
-import { RedCore } from "../core";
+import { ServiceMetadata } from 'typedi';
+import { ServerModuleToken } from '../core/utils';
+import { RedCore } from '../core';
 
 export function ServerModule<T, K extends keyof T>(module: { name: string }) {
   return function (target: Function) {
-    target.prototype.name = module.name
+    target.prototype.name = module.name;
 
     const service: ServiceMetadata<T, K> = {
       type: target,
       multiple: true,
       id: ServerModuleToken,
-      global: false
-    }
+      global: false,
+    };
 
     RedCore.container.set(service);
   };
