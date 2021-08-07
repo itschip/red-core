45b4fc0ff0708cdc03b1279a51f9be1a9f0d9803
Christopher <onlygamernerd@gmail.com>
2021-08-08 00:50:42 +0200

    I need to rethink this architecture

--- server/core/index.ts
+++ server/core/index.ts
@@ -2,14 +2,11 @@ import { ServerModuleProps } from '@typings/modules';
 import RedCoreBase from '../../common/boot';
 import { ServerModuleToken } from './utils';
 import Connection from '../database/connection';
-import PlayerService from '../services/player.service';
 
 export class RedCore extends RedCoreBase {
   protected modules: Map<string, ServerModuleProps>;
 
   public database = Connection.get();
-
-  public playerService = new PlayerService(this);
 
   constructor() {
     super();
--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,5 @@
 // Start coding
 import './database/connection';
-import './modules/player/player.module';
-import './modules/stable/stable.module';
-
 import { RedCore } from './core';
 
 new RedCore().bootstrap();
