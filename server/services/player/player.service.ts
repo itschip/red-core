8d1c11fab3d1707480caecce86d0e01a9a6be7b3
Christopher <onlygamernerd@gmail.com>
2021-08-17 22:55:06 +0200

    update player service'

--- server/services/player/player.service.ts
+++ server/services/player/player.service.ts
@@ -1,18 +1,21 @@
 import { RedCore } from '../../core';
 import { Event, EventListener } from '../../decorators/Events';
+import { ServerModule } from '../../decorators/ServerModule';
 import { getGameLicense } from '../../lib/misc/getGameLicense';
 import PlayerModule from '../../modules/player/player.module';
 
+@ServerModule({ name: 'player' })
 @EventListener()
 export default class PlayerService {
   core: RedCore;
 
   constructor(core: RedCore) {
     this.core = core;
+    console.log('hellloooo', this.core.database.isConnected);
   }
 
   @Event('playerJoining')
-  playerJoining() {
+  private playerJoining() {
     const _source = global.source;
     const license = getGameLicense(_source);
 
@@ -21,5 +24,7 @@ export default class PlayerService {
       source: _source,
       identifier: license,
     });
+
+    console.log('new player is joining', player.getName());
   }
 }
