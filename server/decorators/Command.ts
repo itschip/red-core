31437c50df96c7eb196d460624df8268df2ef4c6
Christopher <onlygamernerd@gmail.com>
2021-09-12 19:59:04 +0200

    i hate reverting stuff

--- server/decorators/Command.ts
+++ server/decorators/Command.ts
@@ -10,6 +10,8 @@ interface Command {
   restricted?: boolean;
 }
 
+console.log('I hate reverting stuff');
+
 export const Command = ({ command, restricted }: Command) => {
   return function (target: unknown, key: string): void {
     if (!Reflect.hasMetadata('commands', target)) {
